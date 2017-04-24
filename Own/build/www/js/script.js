5d454c12395e1847808a66370d11825f538b5fb6
Alexander Bierton <alexanderbierton@googlemail.com>
2017-04-24 08:48:33 +0100

    Basic Jquery functionality

---
+++ Own/build/www/js/script.js
@@ -0,0 +1,20 @@
+var requestUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA4pllhe-rp7ZgsM_a-WIvv3KyC07BL6oM&libraries=places'
+
+$(document).ready(function() {
+  console.log('Ready');
+});
+
+document.addEventListener("deviceReady", function() {
+  console.log('Device Ready')
+});
+
+$(document).on('click', '#bt1', function() {
+  console.log();('Handler for .click() called.');
+});
+
+$(document).on('pagecreate', '#placeList',function(){
+  $.getJSON(requestUrl, function(data) {
+
+  });
+
+});
