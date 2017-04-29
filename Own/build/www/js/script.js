3d894c7d6b73e4a78af3a951c6700596ebda83ab
Alexander Bierton <alexanderbierton@googlemail.com>
2017-04-29 18:02:46 +0100

    added in Place information page with partially working gallery

--- Own/build/www/js/script.js
+++ Own/build/www/js/script.js
@@ -76,7 +76,7 @@ function initmap() {
         console.log(place);
 
         result += "<li>";
-        result+= "<a id='listBtn' onclick='getDetails("+i+")' href='#about' data-transition='pop'>"
+        result+= "<a id='listBtn' onclick='getDetails("+i+")' href='#about' data-transition='slide'>"
         result += "<h1>" + place.name + "</h1>";
         result += "<p>"+place.vicinity+"</p>";
         result == "</a>"
@@ -94,13 +94,97 @@ function initmap() {
 
 function getDetails(pos){
   service.getDetails({ placeId: placeID[pos].place_id }, function(results, status){
-    placeID[pos].details = results
-    console.log(results)
+    placeID[pos].details = results;
+    parseDetails(results);
+
   })
+}
+
+
+function parseDetails(data){
+  console.log(data);
+
+  if(data.photos != undefined) {
+    var covImg = data.photos[0].getUrl({maxWidth: 1000, maxHeight: 1000});
+    disImg = "<img id='coverImg' src='"+covImg+"' />";
+    $("#placeImg").html(disImg);
+
+    var gall = "";
+    var stat = "<h2>Gallery</h2>"
+    var phts = data.photos;
+    for(i = 0; i < phts.length; i++){
+      console.log(data.photos[i]);
+      var galImg = data.photos[i].getUrl({maxWidth: 500, maxHeight: 500});
+      var popImg = data.photos[i].getUrl({maxWidth: 1000, maxHeight: 1000})
+      gall += "<img class='gallery' href='#img"+i+"' src='"+galImg+"' />"
+    }
+    $("#galStatus").html(stat);
+    $("#Gallery").html(gall);
+
+  } else {
+    covImg = "<h3 id='imgMessage'>No Image Availble</h3>"
+    $("#placeImg").html(disImg);
+  }
+
+  if (data.rating != undefined) {
+    var score = data.rating;
+    plScore = "<h2>Rated: "+score+"/5</h2>";
+    $("#rating").html(plScore);
+  } else {
+    plScore = "<h2>No Rating Available</h2>";
+    $("#rating").html(plScore);
+  }
+
+  if(data.geometry.location.lat != undefined && data.geometry.location.lng != undefined){
+    lt = data.geometry.location.lat();
+    ln = data.geometry.location.lng();
+
+    plLtLn = "<a onclick='openMaps("+lt+","+ln+")'><div id='dirBtn' data-icon='navigation' data-iconpos='right'>Directions</div></a>"
+    $("#directions").html(plLtLn);
+    $("#dirBtn").button().button('refresh')
+  } else {
+    plLtLn = "<h3>No location available available</h3>"
+    $("#directions").html(plLtLn);
+  }
+
+  if(data.formatted_phone_number != undefined){
+    var number = data.formatted_phone_number;
+    plNum = "<a href='tel:"+number+"'><div id='phoneBut' data-icon='phone' data-iconpos='right'>Phone</div></a>"
+    $("#phone").html(plNum);
+    $("#phoneBut").button().button('refresh')
+  } else {
+    plNum = "<h3>No Number available</h3>"
+    $("#phone").html(plNum);
+  }
+
+  var title = data.name;
+  plTitle = "<h1>"+title+"</h1>"
+  $("#placeTitle").html(plTitle);
+
+  var add = "<h3>"+data.vicinity+"</h3>";
+  $("#address").html(add);
+
+
+
+}
+
+function backList(){
+  $("#placeImg").empty();
+  $("#placeTitle").empty();
+  $("#rating").empty();
+  $("#address").empty();
+  $("#directions").empty();
+  $("#phone").empty();
+  $("#galStatus").empty();
+  $("#Gallery").empty();
+}
+
+function openMaps(lat, long) {
+  window.open('http://maps.google.com?daddr=' + lat + ',' + long + '&amp;ll=');
 }
 
 function removevar() {
   console.log("Bye");
   $("#results").empty();
-  console.log(map);
+  location.reload(true);
 }
